6bf81e31bb9f15277bdf3b6f3306ee8640ffac1c
mooninbeom <116792524+mooninbeom@users.noreply.github.com>
2023-04-04 16:52:02 +0900

    Create main.dart

---
+++ statefulWidget/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  bool showTitle = true;
+
+  void toggleTitle(){
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              showTitle?MyLargeTitle():Text("nothing to show"),
+              IconButton(onPressed: toggleTitle, icon: Icon(Icons.add)),
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("My Large Title", style: TextStyle(
+      fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color,
+    ),
+    );
+  }
+}
+
