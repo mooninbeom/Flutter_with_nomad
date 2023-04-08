ba2c744078807d78661fec34fb7ba9903e4b350e
mooninbeom <116792524+mooninbeom@users.noreply.github.com>
2023-04-08 20:32:33 +0900

    Create main.dart

---
+++ Today's_Toon/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:study_flutter/screens/home_screen.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+
+
