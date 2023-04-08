189698ff3eb5635ab4f57c0932f3fdeff1186836
mooninbeom <116792524+mooninbeom@users.noreply.github.com>
2023-04-08 20:36:51 +0900

    Create webtoon_episode_model.dart

---
+++ Today's_Toon/models/webtoon_episode_model.dart
@@ -0,0 +1,13 @@
+
+
+class WebtoonEpisodeModel{
+  final String id, title, rating, date;
+
+  WebtoonEpisodeModel.fromJson(Map<String, dynamic> json):
+      id = json['id'],
+      title = json['title'],
+      rating = json['rating'],
+      date = json['date'];
+
+
+}
