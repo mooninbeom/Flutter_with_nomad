5d602eb99c8c439b575c800342c88c856bab645d
mooninbeom <116792524+mooninbeom@users.noreply.github.com>
2023-04-08 20:34:31 +0900

    Create api_service.dart

---
+++ Today's_Toon/services/api_service.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:study_flutter/models/webtoon_detail_model.dart';
+import 'package:study_flutter/models/webtoon_episode_model.dart';
+import 'package:study_flutter/models/webtoon_model.dart';
+
+class ApiService{
+  static const String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
+
+
+  static Future<List<WebtoonModel>> getTodaysToons()async{
+    List<WebtoonModel> toons = [];
+    final url = Uri.parse("$baseUrl/$today");
+    final response = await http.get(url);
+    if(response.statusCode == 200){
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for(var webtoon in webtoons){
+        toons.add(WebtoonModel.fromJson(webtoon));
+      }
+      return toons;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async{
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if(response.statusCode==200){
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLastestEpisodeById(String id) async{
+    List<WebtoonEpisodeModel> episodesInstance = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if(response.statusCode==200){
+      final episodes = jsonDecode(response.body);
+      for(var episode in episodes){
+        episodesInstance.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstance;
+    }
+    throw Error();
+  }
+}
